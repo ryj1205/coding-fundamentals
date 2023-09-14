7a4a792b6af37bc34402d7d3659bdf4503e362a0
Ryan Jackson <ryanjackson8hz@gmail.com>
2023-09-14 15:01:31 +0100

    Tweaked database-example.py to start including more queries

--- python/database-example.py
+++ python/database-example.py
@@ -5,9 +5,14 @@ connectionString = r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=.\SQLExpress;
 conn = pyodbc.connect(connectionString)
 cur = conn.cursor()
 
-sql_query = "INSERT INTO [salesperson] ([emp_no], [first_name],[last_name],[dept_no],[salary],[sales_target],[county],[post_code],[tel],[notes]) VALUES (56, 'Ryan','Jackson',4,1000000,250000,'Derbyshire','S458HZ','01246 266605','These are notes')"
+''' SELECT QUERY '''
 
+''' INSERT QUERY '''
+sql_query = "INSERT INTO [salesperson] ([emp_no], [first_name],[last_name],[dept_no],[salary],[sales_target],[county],[post_code],[tel],[notes]) VALUES (120, 'Ryan','Jackson',4,1000000,250000,'Derbyshire','S458HZ','01246 266605','These are notes')"
 cur.execute(sql_query)
-cur.commit()
+conn.commit()
+
+''' UPDATE QUERY '''
+
 
 conn.close()

494719a6aea2822692024d97b4efc8eceae11884
Ryan Jackson <ryanjackson8hz@gmail.com>
2023-09-14 10:05:42 +0100

    Twekaed car-sales.py to complete exercise.

--- python/lab-exercises/file-io/car-sales.py
+++ python/lab-exercises/file-io/car-sales.py
@@ -1,26 +1,36 @@
 
-# Setup lists ready to populate later:
 companies = []
 sales = []
 
-# Create file object to read from the car.csv file:
-carSales = open("Python/car.csv")
+car_sales_monthly_total = []
+car_sales_company_total = []
 
-for row in carSales:
+# Create file object to read from the car.csv file:
+car_sales = open("Python/car.csv")
+
+# Loop each row in the through the file:
+for row in car_sales:
 
     # Split row data on commas to help work with:
-    parts = row.split(',')
+    data_split = row.split(',')
     
     # Output the company name and append to the companies list:
-    company = parts[0]    
-    companies.append(company)    
+    company_name = data_split[0]    
+    companies.append(company_name)    
 
     # Output the sales figures and append to the sales list:
-    sale_numbers = [int(x) for x in parts[1:]]
-    sales.append(sale_numbers)
+    raw_sales_list = [int(x) for x in data_split[1:]]
+    sales.append(raw_sales_list)
 
-# Output the two lists to ensure data has been captured correctly:
-print("Companies:", companies)
-print("Sales:", sales)
+# Sum of cars sold in each month:
+car_sales_monthly_total = [sum(x) for x in zip(*sales)]
 
-# We are now ready to perform the calculations:
+print(car_sales_monthly_total)
+
+# Total yearly sales by each manufacturer:
+i = 0
+for row in sales:
+    car_sales_company_total.append(companies[i] + " - " + str(sum(row)))
+    i += 1
+
+print(car_sales_company_total)

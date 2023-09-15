1326bf8062cc00164302a7ccc7c34d48b255304f
Ryan Jackson <ryanjackson8hz@gmail.com>
2023-09-15 15:09:34 +0100

    Tweaked polygon.py to include sub-class objects for Square and Rectangle

---
+++ python/lab-exercises/classes/lottery-ball.py
@@ -0,0 +1,9 @@
+
+Goal: 
+“Create a lottery ball, or Hat, that takes a variable number of arguments that specify 
+the number of balls of each color that are in the hat. Give the object the ability to 
+pick a random number of balls from the hat, which will then be used to compute the 
+probability of picking a certain distribution of balls over a large number of experiments”
+
+
+
--- python/lab-exercises/oop/polygon.py
+++ python/lab-exercises/oop/polygon.py
@@ -23,21 +23,58 @@ class Polygon:
 
 ##################################################################
 
+class Square(Polygon):
+
+    def area(self):
+        area_calculation = self.width * self.height
+        return "This area of the square is {}".format(area_calculation)
+
+    def perimeter(self):
+        perimeter_calculation = 2 * (self.height + self.width)
+        return "This perimeter of the square is {}".format(perimeter_calculation)
+
+class Rectangle(Polygon):
+
+    def area(self):
+        area_calculation = self.width * self.height
+        return "This area of the rectangle is {}".format(area_calculation)
+
+    def perimeter(self):
+        perimeter_calculation = 2 * (self.height + self.width)
+        return "This perimeter of the rectangle is {}".format(perimeter_calculation)
+
+##################################################################
+
+square_obj = Square(10, 5)
+print(square_obj.area())
+print(square_obj.perimeter())
+print("")
+
+rectangle_obj = Rectangle(15, 10)
+print(rectangle_obj.area())
+print(rectangle_obj.perimeter())
+print("")
+
+##################################################################
+##################################################################
+##################################################################
+
 class GeometricShapes(Polygon):
 
     def area(self):
         area_calculation = self.width * self.height
-        return area_calculation
+        return "This area of the shape is {}".format(area_calculation)
 
     def perimeter(self):
         perimeter_calculation = 2 * (self.height + self.width)
-        return perimeter_calculation
+        return "This perimeter of the shape is {}".format(perimeter_calculation)
 
 ##################################################################
 
 square = GeometricShapes(10, 5)
 print(square.area())
 print(square.perimeter())
+print("")
 
 rectangle = GeometricShapes(15, 10)
 print(rectangle.area())

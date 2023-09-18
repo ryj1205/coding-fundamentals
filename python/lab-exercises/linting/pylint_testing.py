ac4f48873f18fa49c013b881843f5258a634593b
Ryan Jackson <ryanjackson8hz@gmail.com>
2023-09-18 10:11:56 +0100

    Tweaked formatting

--- python/lab-exercises/linting/pylint_testing.py
+++ python/lab-exercises/linting/pylint_testing.py
@@ -1,6 +1,9 @@
 """ Testing the usage of PyLint """
+
 def count(sequence, item):
+
     """ Count function """
+
     sequence_num = 0
     for current_num in sequence:
         if current_num == item:

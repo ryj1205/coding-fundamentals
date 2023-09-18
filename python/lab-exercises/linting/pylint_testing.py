e1439a55263ea7fb96b208723a5e88db780c54b4
Ryan Jackson <ryanjackson8hz@gmail.com>
2023-09-18 10:14:19 +0100

    10/10 on PyLint

--- python/lab-exercises/linting/pylint_testing.py
+++ python/lab-exercises/linting/pylint_testing.py
@@ -5,7 +5,9 @@ def count(sequence, item):
     """ Count function """
 
     sequence_num = 0
+
     for current_num in sequence:
         if current_num == item:
             sequence_num += 1
+
     return sequence_num

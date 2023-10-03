9a0a5d37c7d27ba7b3d77f9de30c442093ea7d9e
ryj1205 <98744558+ryj1205@users.noreply.github.com>
2023-10-03 08:39:31 +0100

    Delete terraform/lab-exercises directory

--- terraform/lab-exercises/local-files/main.tf
+++
@@ -1,6 +0,0 @@
-# This Terraform script will generate a local file with the given content
-
-resource "local_file" "foo" {
-  content  = "This is a local text file, created using Terraform."
-  filename = "${path.module}/files/terraform-local-file.txt"
-}
